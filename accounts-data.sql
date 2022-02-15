781653ffcbb79807488506429118b4074aa5bfda
David <dgary416@gmail.com>
2022-02-15 10:48:11 -0700

    added starting data, changed schema slightly

---
+++ accounts-data.sql
@@ -0,0 +1,409 @@
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION
+INSERT INTO users
+VALUES (
+        'Alice',
+        'a@alice.com',
+        'alice',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES ('Bob', 'b@bob.com', 'bob', 'secret_password1234');
+INSERT INTO users
+VALUES (
+        'Charlie',
+        'c@charlie.com',
+        'charlie',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Dave',
+        'd@dave.com',
+        'dave',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES ('Eve', 'e@eve.com', 'eve', 'secret_password1234');
+INSERT INTO users
+VALUES (
+        'Frank',
+        'f@reynolds.com',
+        'frank',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Grace',
+        'g@grace.com',
+        'grace',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Heidi',
+        'h@heidi.com',
+        'heidi',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Irene',
+        'i@irene.com',
+        'irene',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Jack',
+        'j@jack.com',
+        'jack',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Karen',
+        'k@karen.com',
+        'karen',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Linda',
+        'l@linda.com',
+        'linda',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Mallory',
+        'm@mallory.com',
+        'mallory',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Nancy',
+        'n@nancy.com',
+        'nancy',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Oscar',
+        'o@oscar.com',
+        'oscar',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Peggy',
+        'p@peggy.com',
+        'peggy',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Quinn',
+        'q@quinn.com',
+        'quinn',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Ruth',
+        'r@ruth.com',
+        'ruth',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Sally',
+        's@sally.com',
+        'sally',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Tina',
+        't@tina.com',
+        'tina',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Ursula',
+        'u@ursula.com',
+        'ursula',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Vince',
+        'v@vince.com',
+        'vince',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Wendy',
+        'w@wendy.com',
+        'wendy',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Xavier',
+        'x@xavier.com',
+        'xavier',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Yvonne',
+        'y@yvonne.com',
+        'yvonne',
+        'secret_password1234'
+    );
+INSERT INTO users
+VALUES (
+        'Zachary',
+        'z@zachary.com',
+        'zachary',
+        'secret_password1234'
+    );
+INSERT INTO posts
+VALUES (
+        'Alice',
+        'Alice says hi.',
+        1,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Bob',
+        'Bob says hi.',
+        2,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Charlie',
+        'Charlie says hi.',
+        3,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Dave',
+        'Dave says hi.',
+        4,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Eve',
+        'Eve says hi.',
+        5,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Frank',
+        'Frank says hi.',
+        6,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Grace',
+        'Grace says hi.',
+        7,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Heidi',
+        'Heidi says hi.',
+        8,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Irene',
+        'Irene says hi.',
+        9,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Jack',
+        'Jack says hi.',
+        10,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Karen',
+        'Karen says hi.',
+        11,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Linda',
+        'Linda says hi.',
+        12,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Mallory',
+        'Mallory says hi.',
+        13,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Nancy',
+        'Nancy says hi.',
+        14,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Oscar',
+        'Oscar says hi.',
+        15,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Peggy',
+        'Peggy says hi.',
+        16,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Quinn',
+        'Quinn says hi.',
+        17,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Ruth',
+        'Ruth says hi.',
+        18,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Sally',
+        'Sally says hi.',
+        19,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Tina',
+        'Tina says hi.',
+        20,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Ursula',
+        'Ursula says hi.',
+        21,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Vince',
+        'Vince says hi.',
+        22,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Wendy',
+        'Wendy says hi.',
+        23,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Xavier',
+        'Xavier says hi.',
+        24,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Yvonne',
+        'Yvonne says hi.',
+        25,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+INSERT INTO posts
+VALUES (
+        'Zachary',
+        'Zachary says hi.',
+        26,
+        0,
+        0,
+        '2022-01-01 00:00:00'
+    );
+COMMIT;
